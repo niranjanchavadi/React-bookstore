97dcaf7e452518ddbbe95446917e0814b791f3f1
vinay <vinaygangishetty96@gmail.com>
2020-06-23 23:48:17 +0530

    [vinay]ADDED Registration Component

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,17 +1,29 @@
 import React from "react";
-import {BrowserRouter, Route} from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Login from '../component/Login';
 import ForgotPassword from "../component/ForgotPassword";
- 
-const Router = () => {
-  return (
-    <BrowserRouter>
-      <Route path="/forgotpassword" component={ForgotPassword} />
-      <Route path="/" exact component={Login} />
-      <Route path="/login" exact component={Login} />
+import Registration from "../component/Registration";
 
-   </BrowserRouter>
-  );
+const Router = () => {
+    return ( <
+        BrowserRouter >
+        <
+        Route path = "/register"
+        component = { Registration }
+        /> <
+        Route path = "/forgotpassword"
+        component = { ForgotPassword }
+        /> <
+        Route path = "/"
+        exact component = { Login }
+        /> <
+        Route path = "/login"
+        exact component = { Login }
+        />
+
+        <
+        /BrowserRouter>
+    );
 };
 
 export default Router;
