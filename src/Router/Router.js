21c597321108c88b57a6ff144f15eeb84106581b
vinay <vinaygangishetty96@gmail.com>
2020-07-02 14:34:38 +0530

    [vinay]REFACTORED: Router.js page

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -4,18 +4,35 @@ import Login from '../component/Login';
 import ForgotPassword from "../component/ForgotPassword";
 import Registration from "../component/Registration";
 import ResetPassword from "../component/ResetPassword";
+import UserDashboard from "../component/UserDashboard";
+import SellerPage from "../component/SellerPage";
 
 const Router = () => {
-    return ( 
-     <BrowserRouter >
-        
-        <Route path = "/register" component = { Registration }  />
-        <Route path = "/login"  exact component = { Login } />
-        <Route path = "/forgotpassword" component = { ForgotPassword } /> 
-        <Route path="/resetpassword" component={ResetPassword } />
-        <Route path = "/"  exact component = { Login }  />
-        
-    </BrowserRouter>
+    return ( <
+        BrowserRouter >
+
+        <
+        Route path = "/register"
+        component = { Registration }
+        /> <
+        Route path = "/login"
+        exact component = { Login }
+        /> <
+        Route path = "/forgotpassword"
+        component = { ForgotPassword }
+        />  <
+        Route path = "/resetpassword"
+        component = { ResetPassword }
+        /> <
+        Route path = "/"
+        exact component = { UserDashboard }
+        /> <
+        Route path = "/seller"
+        exact component = { SellerPage }
+        />
+
+        <
+        /BrowserRouter>
     );
 };
 
